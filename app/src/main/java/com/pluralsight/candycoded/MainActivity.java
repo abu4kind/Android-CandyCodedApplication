f9a9c8e755995aed82fa60eb624b6034cfcbb414
abu4kind <aadamu0298@gmail.com>
2021-11-20 16:55:39 +0100

    Create the Intent for Launching the InfoActivity Modified

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -31,13 +31,6 @@ public class MainActivity extends AppCompatActivity {
   private CandyDbHelper candyDbHelper = new CandyDbHelper(this);
 
   @Override
-  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
-    startActivity(infoIntent);
-    return super.onOptionsItemSelected(item);
-  }
-
-  @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -91,6 +84,14 @@ public class MainActivity extends AppCompatActivity {
   // ***
   // TODO - Task 1 - Show Store Information Activity
   // ***
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Intent infoIntent = new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+
+    return super.onOptionsItemSelected(item);
+  }
 
   private void addCandiesToDatabase(Candy[] candies) {
     SQLiteDatabase db = candyDbHelper.getWritableDatabase();
